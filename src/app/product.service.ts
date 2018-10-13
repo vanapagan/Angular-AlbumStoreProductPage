7f0f65db95a579c1db06ed07047d2880d49cae84
Kristo Palo <kristo.palo@gmail.com>
2018-10-13 22:28:40 +0300

    Update src/app/product.service.ts

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -11,7 +11,7 @@ export class ProductService {
 
   constructor(private _http: Http) { }
 
-  getAlbum(id: number): Observable<Response> {
+  getAlbum(id: number) {
     return this._http.get(this._albumUrl).map(response => response.json());
   }
 
